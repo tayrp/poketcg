c0d6a114a1fae0228f629e5f00852b90fd6670ff
Tay <149682424+tayrp@users.noreply.github.com>
2024-03-15 04:24:00 -0400

    Delete poketcg.py

--- poketcg.py
+++
@@ -1,8 +0,0 @@
-from pokemontcgsdk import Card
-from pokemontcgsdk import Set
-from pokemontcgsdk import Type
-from pokemontcgsdk import Supertype
-from pokemontcgsdk import Subtype
-from pokemontcgsdk import Rarity
-
-export POKEMONTCG_IO_API_KEY='5be6b7cb-0bdc-4053-9db7-080758b8d6de'

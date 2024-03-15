1ef9a19eb93666405bc408b9782f3733be24cd07
tayrp <taylor.nichols08@icloud.com>
2024-03-15 04:31:19 -0400

    Main source edits !!!

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import discord
-import asyncio
-from discord.ext import commands
+from typing import Final
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from responses import get_response
 from pokemontcgsdk import Card
 from pokemontcgsdk import Set
 from pokemontcgsdk import Type
@@ -8,7 +10,62 @@ from pokemontcgsdk import Supertype
 from pokemontcgsdk import Subtype
 from pokemontcgsdk import Rarity
 
-export POKEMONTCG_IO_API_KEY='5be6b7cb-0bdc-4053-9db7-080758b8d6de'
+# PokemonTCG.io API Key
+export POKEMONTCG_IO_API_KEY='12345678-1234-1234-1234-123456789ABC'
 
-# Discord Bot Status
-await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="pcg info"))
+# STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+
+# STEP 1: BOT SETUP
+intents: Intents = Intents.default()
+intents.message_content = True  # NOQA
+client: Client = Client(intents=intents)
+
+
+# STEP 2: MESSAGE FUNCTIONALITY
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('(Message was empty because intents were not enabled probably)')
+        return
+
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+# STEP 3: HANDLING THE STARTUP FOR OUR BOT
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
+
+
+# STEP 4: HANDLING INCOMING MESSAGES
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: "{user_message}"')
+    await send_message(message, user_message)
+
+
+# STEP 5: MAIN ENTRY POINT
+def main() -> None:
+    client.run(token=TOKEN)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
